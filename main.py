30057b22fa026d99a22915c8dcd2b98af67bde97
Erick Ricardo <e.ricardo@iesb.edu.br>
2025-01-10 19:47:29 -0300

    adicionando if else

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ print(url)
 
 
 nome = "Erick"
+print('você é o Erick?')
+nome_digitado = input("Digite seu nome: ")
+
+if nome_digitado == nome:
+    print("Seu nome é",nome)
+else:
+    print("Seu nome não é ",nome)  # Seu nome é Erick
