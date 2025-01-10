f5ab0c21e04d06023ab84e6a8836c67afd4c8e9d
Erick Ricardo <e.ricardo@iesb.edu.br>
2025-01-10 20:24:11 -0300

    Revert "adicionando if else"
    This reverts commit 30057b22fa026d99a22915c8dcd2b98af67bde97.

--- main.py
+++ main.py
@@ -4,10 +4,3 @@ print(url)
 
 
 nome = "Erick"
-print('você é o Erick?')
-nome_digitado = input("Digite seu nome: ")
-
-if nome_digitado == nome:
-    print("Seu nome é",nome)
-else:
-    print("Seu nome não é ",nome)  # Seu nome é Erick
